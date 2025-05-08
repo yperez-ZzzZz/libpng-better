db54b1880d089952d3110cbd317148d87fac1664
Agathe Claire Denyse Jeanne Pelayo <ariel.pelayo@epfl.ch>
2025-05-08 16:36:50 +0200

    run fuzzer without seed corpus

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -42,9 +42,5 @@ $CXX $CXXFLAGS -std=c++11 -I. \
      -o $OUT/libpng_read_fuzzer \
      -lFuzzingEngine .libs/libpng16.a -lz
 
-# add seed corpus.
-find $SRC/libpng -name "*.png" | grep -v crashers | \
-     xargs zip $OUT/libpng_read_fuzzer_seed_corpus.zip
-
 cp $SRC/libpng/contrib/oss-fuzz/*.dict \
      $SRC/libpng/contrib/oss-fuzz/*.options $OUT/
